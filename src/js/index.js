ef99923d010b90c74a194ae54108b92ad96ab35f
Alexandra Huliakevich <sunnykuzia1996@gmail.com>
2024-05-23 13:16:04 +0300

    feat: animate tabs

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabheader = document.querySelector('.tabheader'),
+        items = tabheader.querySelectorAll('.tabheader__item'),
+        contents = document.querySelectorAll('.tabcontent');
+
+
+
+    function setActiveItem(target = items[0]) {
+        items.forEach((item, i) => {
+            item.classList.add('fade');
+            item.classList.remove('tabheader__item_active');
+            if (target === item) {
+                target.classList.add('tabheader__item_active');
+                displayContent(i);
+            }
+        })
+
+    }
+    setActiveItem();
+
+    function displayContent(i) {
+        contents.forEach(content => {
+            content.classList.add('fade');
+            content.classList.add('tabcontent__hidden');
+        })
+        contents[i].classList.remove('tabcontent__hidden');
+    }
+
+    tabheader.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target && target.matches('.tabheader__item')) {
+            setActiveItem(target);
+        }
+    });
+});
